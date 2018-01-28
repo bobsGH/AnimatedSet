f5070ee56330e498bdbd4bee15a732fd968ecb7f
Robert Anderson <bobsnet1@me.com>
2018-01-28 09:44:42 -0800

    added function to print grid characteristics

--- Set3/ViewController.swift
+++ Set3/ViewController.swift
@@ -13,10 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        grid.frame = CGRect(x: 8, y: 8, width: 200, height: 250)
+        grid.cellCount = 12
+        
+        tstGrid()
+        
     }
 
-    let grid = Grid(layout: Grid.Layout.aspectRatio(0.7) )
+    var grid = Grid(layout: Grid.Layout.aspectRatio(0.7) )
     
+    func tstGrid() {
+        let gridN = grid.cellCount
+        let grid2 = grid[2]
+        let dimension = grid.dimensions  // rowCount, column dount
+        let cellSize = grid.cellSize
+         print("cell count \(gridN) \n cell Rect  \(grid2) \n dimension \(dimension) \ncell Size \(cellSize)")
+        
+    }
+    
+
 
 }
 
