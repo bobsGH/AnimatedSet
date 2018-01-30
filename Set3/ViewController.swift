5f92f02bbfb8198b1eefb7f09e304f80e787b2f9
Robert Anderson <bobsnet1@me.com>
2018-01-30 10:34:25 -0800

    vertical grid working in paint()

--- Set3/ViewController.swift
+++ Set3/ViewController.swift
@@ -13,10 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        grid.frame = CGRect(x: 8, y: 8, width: 200, height: 250)
+       // grid.frame = CGRect(x: 8, y: 8, width: 200, height: 250)
+       // grid.frame = containerView.frame
+        grid.frame = containerView.bounds
+        print("containerView.frame \(containerView.frame)")
         grid.cellCount = 12
+    
         
-        tstGrid()
+       // tstGrid()
+        paintCards()
         
     }
     
@@ -44,17 +49,19 @@ class ViewController: UIViewController {
         object.layer.cornerRadius = 8.0
     }
     
-    
+ 
     
     func tstGrid() {
         let gridN = grid.cellCount
         let grid2 = grid[2]
         let dimension = grid.dimensions  // rowCount, column dount
         let cellSize = grid.cellSize
-         print("cell count \(gridN) \n cell Rect  \(grid2) \n dimension \(dimension) \ncell Size \(cellSize)")
+        print("grid.cellCount \(gridN) \n grid2  \(grid2) \n grid.dimension \(dimension) \n grid.cellSize \(cellSize)")
         
         var containerSize = containerView.frame
-        print("container size \(containerSize)")
+        //print("container size \(containerSize)")
+        
+       // print("grid2 \(grid2)")
         
         let cardSubView = CardView()
         var cardRect = CGRect(x: 108.0, y: 0.0, width: 50.0, height: 71.4)
@@ -63,18 +70,32 @@ class ViewController: UIViewController {
         cardSubView.frame = cardRect
         cardSubView.backgroundColor = .yellow
         
-        
         roundCornor(score)
-        
         
         // self.view = view
        // view.addSubview(cardSubView)
         containerView.addSubview(cardSubView)
-       
-    
     }
+ 
     
-
+    func paintCards() {
+        
+        for i in 0..<grid.cellCount  {
+            var cardView = CardView()
+          
+            var cardRect = grid[i]
+         
+            cardView.frame = cardRect!
+            cardView.backgroundColor = .green
+            roundCornor(cardView)
+            //cardView = cardRect
+           // print("cardView frame \(cardView.frame)\n cardView \(cardView)\n")
+          
+            
+            containerView.addSubview(cardView)
+            
+        }
+    }
 
 }
 
